a1b154c9f79954d97b45243118a2d9ab04c4ffeb
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2021-03-26 09:42:58 -0300

    save the inputs

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from os import path
 from openpyxl import load_workbook
 
 today = datetime.now()
+date = datetime(today.year, today.month, today.day)
+
 
 print(f"Hoje é {today.day}/{today.month}/{today.year}")
 answer = input("[S / N]   ")
+# if answer.upper() == 'S':
+    # date = datetime(today.year, today.month, today.day)
 
 print(answer)
 
@@ -40,7 +44,7 @@ columns = [
     "OBITOS"
 ]
 
-data = [f"{today.day}/{today.month}/{today.year}"]
+data = [date]
 
 for field in fields:
     answer = input(field)
@@ -50,19 +54,28 @@ for field in fields:
         value = 0
     data.append(value)
 
+print(data)
 
 df = pd.DataFrame([data], columns=columns)
+
 # df.to_excel("teste.xlsx", index=False)
 ## https://betterprogramming.pub/using-python-pandas-with-excel-d5082102ca27
-infile = path.join("data", "raw", "BOLETIM_DIARIO_CORONAVIRUS_SAP.xlsx")
-writer = pd.ExcelWriter(infile, engine='openpyxl')
+in_excel = path.join("data", "raw", "BOLETIM_DIARIO_CORONAVIRUS_SAP.xlsx")
+
+writer = pd.ExcelWriter(in_excel, engine='openpyxl')
 # try to open an existing workbook
-writer.book = load_workbook(infile)
+writer.book = load_workbook(in_excel)
+
 # copy existing sheets
-writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
-# read existing file
-reader = pd.read_excel(infile, engine="openpyxl")
-# write out the new sheet
-df.to_excel(writer, index=False,header=False,startrow=len(reader)+1)
+writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets if ws.title != "Sheet1")
+
+reader = pd.read_excel(in_excel, engine="openpyxl")
+reader.dropna(how='all')
+
+df.to_excel(writer,
+            sheet_name="Planilha1",
+            index=False,
+            header=False,
+            startrow=len(reader))
 
 writer.close()
