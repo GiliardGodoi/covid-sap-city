e838e1555169a98c8397f3172619c2f9442609ce
Giliard Godoi <giliardgodoi@alunos.utfpr.edu.br>
2021-04-20 16:27:14 -0300

    add feed function

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,12 @@
 import typer
 from os import path
 from rich.console import Console
+from datetime import datetime
+
+import pandas as pd
+from rich.traceback import install
+
+from src.questioner import ask4YesOrNo, ask4date, ask4int, ask4option, greeting
 
 
 from src.charts import (timeseries_compare_confirmed_understudy,
@@ -13,6 +19,8 @@ from src.charts import (timeseries_compare_confirmed_understudy,
                         )
 from src.storage import read_excel
 
+install()
+
 app = typer.Typer()
 console = Console()
 
@@ -23,7 +31,59 @@ charts_folder = path.join('data', 'charts')
 def feed(many : bool = typer.Option(False,
                                     is_flag=True,
                                     help="Use essa flag para inserir dados para várias datas.")):
-    pass
+
+
+    fields = [
+        "Data",
+        "Descartados",
+        "Em investigação",
+        "Confirmados",
+        "Examinados",
+        "Recuperados",
+        "Ativos",
+        "Hospitalizados",
+        "Domicílio",
+        "Óbitos"
+    ]
+
+    columns = [
+        "DATA",
+        "DESCARTADOS",
+        "EM INVESTIGACAO",
+        "CONFIRMADOS",
+        "EXAMINADOS",
+        "RECUPERADOS",
+        "ATIVOS",
+        "HOSPITAL",
+        "DOMICILIO",
+        "OBITOS"
+    ]
+
+    msg = '''
+    # Boletins Diário Santo Antônio da Platina - PR
+    Utilitário para coletar os dados dos boletins diários sobre os casos de *COVID-19* no Município de Santo Antônio da Platina - PR
+    '''
+
+    greeting(msg)
+
+    width = max(len(f) for f in fields) + 2
+
+    data = list()
+
+    go_on = True
+
+    while go_on:
+        line = list()
+        for i, field in enumerate(fields):
+            if i == 0:
+                ans = ask4date(f"{field}:", justify='left', width=width, default=datetime.now())
+            else:
+                ans = ask4int(f"{field}:", justify='left', width=width, default=0)
+            line.append(ans)
+        go_on = ask4YesOrNo("Deseja continuar?")
+        data.append(line)
+
+    print(data)
 
 @app.command()
 def charts(source : str = typer.Argument(data_source, help='Arquivo que contem os dados'),
